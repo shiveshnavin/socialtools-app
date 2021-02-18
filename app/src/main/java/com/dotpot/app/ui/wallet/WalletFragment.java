5361b8270c125c817ce5eabf675eb65503d1bed0
shiveshnavin <shiveshnavin@gmail.com>
2021-02-18 22:41:04 +0530

    not wokring tabs

--- app/src/main/java/com/dotpot/app/ui/wallet/WalletFragment.java
+++ app/src/main/java/com/dotpot/app/ui/wallet/WalletFragment.java
@@ -69,89 +69,102 @@ public class WalletFragment extends BaseFragment {
      * (http://www.buzzingandroid.com/tools/android-layout-finder)
      */
     private void findViews(View root) {
-        contWalletBalancecont = (ConstraintLayout)root.findViewById( R.id.contWalletBalancecont );
-        contWalletBalance = (LinearLayout)root.findViewById( R.id.contWalletBalance );
-        currency = (TextView)root.findViewById( R.id.currency );
-        walletBalance = (TextView)root.findViewById( R.id.walletBalance );
-        yourWalletBalanceTxt = (TextView)root.findViewById( R.id.yourWalletBalanceTxt );
-        yourWalletBalanceSep = (View)root.findViewById( R.id.yourWalletBalanceSep );
-        contaddMoneyBalance = (ConstraintLayout)root.findViewById( R.id.contaddMoneyBalance );
-        addMoneyBal = (TextView)root.findViewById( R.id.addMoneyBal );
-        addMoneyBalTxt = (TextView)root.findViewById( R.id.addMoneyBalTxt );
-        addMoneyBalIcon = (RoundRectCornerImageView)root.findViewById( R.id.addMoneyBalIcon );
-        next = (RoundRectCornerImageView)root.findViewById( R.id.next );
-        addBtn = (Button)root.findViewById( R.id.addBtn );
-        withdrawBtn =  root.findViewById( R.id.withdrawBtn );
-        contNumbers = (LinearLayout)root.findViewById( R.id.contNumbers );
-        contAwardBalance = (ConstraintLayout)root.findViewById( R.id.editProfileCont);
-        awardBalAction = (Button)root.findViewById( R.id.awardBalAction );
-        awardBalTxt = (TextView)root.findViewById( R.id.awardBalTxt );
-        awardBal = (TextView)root.findViewById( R.id.editProfileTxt);
-        awardBalIcon = (RoundRectCornerImageView)root.findViewById( R.id.awardBalIcon );
-        contRefBalance = (ConstraintLayout)root.findViewById( R.id.contRefBalance );
-        refBal = (TextView)root.findViewById( R.id.refBal );
-        refBalTxt = (TextView)root.findViewById( R.id.refBalTxt );
-        refBalIcon = (RoundRectCornerImageView)root.findViewById( R.id.refBalIcon );
-        recentTxnTxt = (TextView)root.findViewById( R.id.recentTxnTxt );
-        tabTxns = (TabLayout)root.findViewById( R.id.tabTxns );
-        tabAll = (TabItem)root.findViewById( R.id.tab_all );
-        tabCredit = (TabItem)root.findViewById( R.id.tab_credit );
-        tabDebit = (TabItem)root.findViewById( R.id.tab_debit );
-        listTransactions = (RecyclerView)root.findViewById( R.id.listTransactions );
+        contWalletBalancecont = (ConstraintLayout) root.findViewById(R.id.contWalletBalancecont);
+        contWalletBalance = (LinearLayout) root.findViewById(R.id.contWalletBalance);
+        currency = (TextView) root.findViewById(R.id.currency);
+        walletBalance = (TextView) root.findViewById(R.id.walletBalance);
+        yourWalletBalanceTxt = (TextView) root.findViewById(R.id.yourWalletBalanceTxt);
+        yourWalletBalanceSep = (View) root.findViewById(R.id.yourWalletBalanceSep);
+        contaddMoneyBalance = (ConstraintLayout) root.findViewById(R.id.contaddMoneyBalance);
+        addMoneyBal = (TextView) root.findViewById(R.id.addMoneyBal);
+        addMoneyBalTxt = (TextView) root.findViewById(R.id.addMoneyBalTxt);
+        addMoneyBalIcon = (RoundRectCornerImageView) root.findViewById(R.id.addMoneyBalIcon);
+        next = (RoundRectCornerImageView) root.findViewById(R.id.next);
+        addBtn = (Button) root.findViewById(R.id.addBtn);
+        withdrawBtn = root.findViewById(R.id.withdrawBtn);
+        contNumbers = (LinearLayout) root.findViewById(R.id.contNumbers);
+        contAwardBalance = (ConstraintLayout) root.findViewById(R.id.editProfileCont);
+        awardBalAction = (Button) root.findViewById(R.id.awardBalAction);
+        awardBalTxt = (TextView) root.findViewById(R.id.awardBalTxt);
+        awardBal = (TextView) root.findViewById(R.id.editProfileTxt);
+        awardBalIcon = (RoundRectCornerImageView) root.findViewById(R.id.awardBalIcon);
+        contRefBalance = (ConstraintLayout) root.findViewById(R.id.contRefBalance);
+        refBal = (TextView) root.findViewById(R.id.refBal);
+        refBalTxt = (TextView) root.findViewById(R.id.refBalTxt);
+        refBalIcon = (RoundRectCornerImageView) root.findViewById(R.id.refBalIcon);
+        recentTxnTxt = (TextView) root.findViewById(R.id.recentTxnTxt);
+        tabTxns = (TabLayout) root.findViewById(R.id.tabTxns);
+        tabAll = tabTxns.findViewById(R.id.tab_all);
+        tabCredit = tabTxns.findViewById(R.id.tab_credit);
+        tabDebit = tabTxns.findViewById(R.id.tab_debit);
+        listTransactions = (RecyclerView) root.findViewById(R.id.listTransactions);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater,container,savedInstanceState);
+        super.onCreateView(inflater, container, savedInstanceState);
         walletViewModel = WalletViewModel.getInstance();
         View root = inflater.inflate(R.layout.fragment_wallet, container, false);
         findViews(root);
         try {
-            if (act.fragmentManager.getBackStackEntryCount() > 0)
-            {
+            if (act.fragmentManager.getBackStackEntryCount() > 0) {
                 act.fragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
             }
         } catch (Exception e) {
             utl.e("Minor err at WalletFragment:115");
-           // e.printStackTrace();
+            // e.printStackTrace();
         }
 
         walletViewModel.getTransactions().observe(getViewLifecycleOwner(), this::setUpTransactionsList);
         walletViewModel.getWallet().observe(getViewLifecycleOwner(), this::setUpWallet);
         addBtn.setOnClickListener(view -> navService.startAddCredits(fragmentId));
 
-        tabAll.setOnClickListener(v->{
-            walletViewModel.refresh(null);
+        tabTxns.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getText().toString().contains(getString(R.string.credit))) {
+                    walletViewModel.refresh("wallet_credit");
+                } else if (tab.getText().toString().contains(getString(R.string.debit))) {
+                    walletViewModel.refresh("wallet_debit");
+                } else {
+                    walletViewModel.refresh(null);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
         });
-        tabCredit.setOnClickListener(v->{
-            walletViewModel.refresh("wallet_credit");
-        });
-        tabDebit.setOnClickListener(v->{
-            walletViewModel.refresh("wallet_debit");
-        });
+
 
         return root;
     }
 
     private void setUpWallet(Wallet wallet) {
 
-        walletBalance.setText(""+wallet.getCreditBalance());
-        awardBal.setText(getString(R.string.award_bal)+getString(R.string.currency)+" "+wallet.getWinningBalance());
-        awardBal.setText(getString(R.string.ref_bal)+getString(R.string.currency)+" "+wallet.getAggReferralBalance());
+        walletBalance.setText("" + wallet.getCreditBalance());
+        awardBal.setText(getString(R.string.award_bal) + getString(R.string.currency) + " " + wallet.getWinningBalance());
+        awardBal.setText(getString(R.string.ref_bal) + getString(R.string.currency) + " " + wallet.getAggReferralBalance());
 
     }
 
-    private void setUpTransactionsList(List<Transaction> transactionsList){
+    private void setUpTransactionsList(List<Transaction> transactionsList) {
 
-        adapter = new GenriXAdapter<Transaction>(getContext(),R.layout.row_transaction,transactionsList){
+        adapter = new GenriXAdapter<Transaction>(getContext(), R.layout.row_transaction, transactionsList) {
             @Override
             public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
 
                 final int pos = viewHolder.getAdapterPosition();
-                final CustomViewHolder vh = (CustomViewHolder)viewHolder;
+                final CustomViewHolder vh = (CustomViewHolder) viewHolder;
                 final Transaction transaction = transactionsList.get(pos);
-                vh.textView(R.id.txnId).setText("ID #"+transaction.getId());
-                vh.textView(R.id.txnAmtTxt).setText(getString(R.string.currency)+" "+transaction.getAmount());
+                vh.textView(R.id.txnId).setText("ID #" + transaction.getId());
+                vh.textView(R.id.txnAmtTxt).setText(getString(R.string.currency) + " " + transaction.getAmount());
                 vh.textView(R.id.txnStatusTxt).setText(transaction.getStatus());
 
             }
